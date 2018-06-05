ed052684e1389e3c64c9458e7f04209b67ebef09
Scaitux <39919830+Scaitux@users.noreply.github.com>
2018-06-05 22:06:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = "NDUxOTc1MzY0MTIyNTA5MzEy.DfJmYA.fXxMbEhEUO1wxO0vxGhwoXUQTpg"
 
 client.on('ready', () => {
   client.user.setGame('Type || ^^help ', 'https://www.twitch.tv/streamerhouse')
